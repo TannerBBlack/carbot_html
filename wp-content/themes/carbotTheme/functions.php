b514f64c56f0220df941d2def94686865e332c44
Tanner Black <tbb777@gmail.com>
2021-03-10 16:58:36 -0700

    removed view funtion

--- wp-content/themes/carbotTheme/functions.php
+++ wp-content/themes/carbotTheme/functions.php
@@ -1,6 +1,6 @@
 <?php>
 //remove visual editor
-add_filter( 'user_can_richedit' , '__return_false', 50 );
+// add_filter( 'user_can_richedit' , '__return_false', 50 );
 
 // create a menu
 // register_nav_menu( 'siteNavigation', 'Primary Navigation' );
