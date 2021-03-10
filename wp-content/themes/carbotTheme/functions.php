a92d5139733429e6c7578f721544e021e0982a4b
Tanner Black <tbb777@gmail.com>
2021-03-10 16:57:22 -0700

    remove menu function

--- wp-content/themes/carbotTheme/functions.php
+++ wp-content/themes/carbotTheme/functions.php
@@ -3,6 +3,6 @@
 add_filter( 'user_can_richedit' , '__return_false', 50 );
 
 // create a menu
-register_nav_menu( 'siteNavigation', 'Primary Navigation' );
+// register_nav_menu( 'siteNavigation', 'Primary Navigation' );
 
 ?>
