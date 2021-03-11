bd6c2dcdb008094b4f75581929dd3f0f6dd42aa7
Tanner Black <tbb777@gmail.com>
2021-03-10 17:11:43 -0700

    trying to fix menu

--- wp-content/themes/carbotTheme/functions.php
+++ wp-content/themes/carbotTheme/functions.php
@@ -1,8 +1,8 @@
 <?php
 //remove visual editor
-// add_filter( 'user_can_richedit' , '__return_false', 50 );
+add_filter( 'user_can_richedit' , '__return_false', 50 );
 
 // create a menu
-// register_nav_menu( 'siteNavigation', 'Primary Navigation' );
+register_nav_menu( 'siteNavigation', 'Primary Navigation' );
 
 ?>
