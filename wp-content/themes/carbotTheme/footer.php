6c05baad2dea0f210b93eae58118617572788794
Tanner Black <tbb777@gmail.com>
2021-03-17 12:14:03 -0600

    added header and footer

--- wp-content/themes/carbotTheme/footer.php
+++ wp-content/themes/carbotTheme/footer.php
@@ -1,2 +1,63 @@
+<div class="full-width-footer">
+  <footer>
+    <div class="flexed">
+      <div class="column small-12 medium-6 large-4">
+        <h2>Contact Us</h2>
+        <p>CABOT CRUIZES<br>
+        23 South Main St. Suite 16<br>
+        Lexington, VA 24450</p>
+        <p>PHONE:<br>
+        1-800-123-4567</p>
+        <p>HOURS OF OPERATION:<br>
+          Monday - Friday 9am-4pm</p>
+      </div>
+
+      <div class="column small-12 medium-6 large-4">
+        <h2>News Letter</h2>
+        <p>Subscribe to our ameail list and stay up-to-date with our hottest offers and latest specials.</p>
+        <form>
+          <input type="email">
+          <input type="submit" value="Subscribe">
+        </form>
+      </div>
+
+      <div class="column small-12 medium-12 large-4">
+        <h2>Why Cobot Cruises</h2>
+        <p>Cabot Cruises is a travel agency providing the best cruise
+          deals on our online travel website. We can help you with all
+          inclusive vacaMons including discounted cruises.</p>
+        <p>We do not sell travel to residents of Delaware, Iowa,
+          Florida, Hawaii and Washington state because those states
+          are just wierd. If you are a resident of one of these states,
+          call your congressman and tell them to change the
+          regulaMons.</p>
+      </div>
+    </div>
+    <div class="social">
+      <a class="fab fa-facebook-square"></a>
+      <a class="fab fa-twitter-square"></a>
+      <a class="fab fa-instagram"></a>
+      <a class="fab fa-tumblr-square"></a>
+    </div>
+  &copy; Tanner Black 2021
+  </footer>
+</div>
+
+<script src="js/script.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('.your-class').slick({
+      dots: true,
+      arrows: false,
+      autoplay: true,
+    });
+  });
+</script>
+
 </body>
 </html>
--- wp-content/themes/carbotTheme/header.php
+++ wp-content/themes/carbotTheme/header.php
@@ -1,10 +1,67 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
+<meta name="description" content="Fill Me In">
+<meta name="author" content="John Doe">
 <title>Carbot Cruise<?php wp_title(); ?></title>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
-</head>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;700&display=swap" rel="stylesheet">
 
+<!-- TELLS PHONES NOT TO LIE ABOUT THEIR WIDTH & stops the font from enlarging when a phone is turned sideways-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+
+<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
+<!--Add the new slick-theme.css if you want the default styling --> 
+<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
+
+
+<!-- STYLE SHEETS -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+
+</head>
 <body>
-<?php wp_nav_menu()?>
+
+<header class="clearfix">
+  <figure class="logo">
+    <img srcset="images/logo1.png 1x, images/logo2.png 2x, images/logo3.png 3x" 
+    src="images/logo1.png" alt="logo">
+  </figure>
+  <a href="tel:123-456-7890">123-456-7890</a>
+</header>
+
+<div class="full-width-nav">
+  <nav class="clearfix">
+    <button id="hamburgerBtn">
+      <span class="line top"></span>
+      <span class="line middle"></span>
+      <span class="line bottom"></span>
+    </button>
+
+    <!-- <ul id="primaryNav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="#">Cruises</a></li>
+        <li class="menu-item-has-children"><a href="#">Agents</a>
+          <ul>
+            <li><a href="#">Sally Radgers</a></li>
+            <li><a href="#">Bobby Sorrell</a></li>
+          </ul>
+        </li>
+      <li><a href="#">Book</a></li>
+    </ul> -->
+    <?php wp_nav_menu()?>
+
+
+    <div class="wrap">
+      <div class="search">
+         <input type="text" class="searchTerm" placeholder="Search...">
+         <button type="submit" class="searchButton">
+           <i class="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
+
+  </nav>
+</div>
