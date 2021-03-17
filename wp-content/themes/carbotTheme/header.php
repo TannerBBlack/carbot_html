3e1872002942741598c9b5e8d3eb6e697512c7e9
Tanner Black <tbb777@gmail.com>
2021-03-17 12:06:26 -0600

    testing first page and stylsheet

--- wp-content/themes/carbotTheme/header.php
+++ wp-content/themes/carbotTheme/header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Untitled Document</title>
+<title>Carbot Cruise<?php wp_title(); ?></title>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
 </head>
 
 <body>
--- wp-content/themes/carbotTheme/index.php
+++ wp-content/themes/carbotTheme/index.php
@@ -1,3 +1,40 @@
-<?php
-// required for a blog
-?>
+<?php include 'header.php'; ?>
+<main>
+    <div class="your-class">
+        <picture><source media="(max-width: 450px" srcset="images/cruise1small.jpg"><img src="images/cruise1.jpg"></picture>
+        <picture><source media="(max-width: 450px" srcset="images/cruise2small.jpg"><img src="images/cruise2.jpg"></picture>
+        <picture><source media="(max-width: 450px" srcset="images/carnival-magic-ship-aerial-featsmall.jpg"><img src="images/carnival-magic-ship-aerial-feat.jpg"></picture>
+        <picture><source media="(max-width: 450px" srcset="images/cruise-ship-2small.jpg"><img src="images/cruise-ship-2.jpg"></picture>
+    </div>
+
+    <h2>Recent Blog Posts</h2>
+
+    <div class="blog-posts">
+        <div class="card column small-6 medium-4 large-3">
+            <figure>
+                <a href="#"><img src="images/room.jpg" alt="beach"></a>
+            </figure>
+            <p class="title">Find the best vacation!</p>
+        </div>
+        <div class="card column small-6 medium-4 large-3">
+            <figure>
+                <a href="#"><img src="images/fruit.jpg" alt="beach"></a>
+            </figure>
+            <p class="title">Try these top local food!</p>
+        </div>
+        <div class="card column small-6 medium-4 large-3">
+            <div class="post-title"></div>
+            <figure>
+                <a href="#"><img src="images/restraunt.jpg" alt="beach"></a>
+            </figure>
+            <p class="title">Pick the best restraunts</p>
+        </div>
+        <div class="card column small-6 medium-4 large-3">
+            <figure>
+                <a href="#"><img src="images/beach.jpg" alt="beach"></a>
+            </figure>
+            <p class="title">Best beach in the caribean</p>
+        </div>
+    </div>
+</main>
+<?php include 'footer.php'; ?>
